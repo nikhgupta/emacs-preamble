709505623236fa27ee678a9641da0af6f7fc85f6
Nikhil Gupta <me@nikhgupta.com>
2013-09-22 00:08:57 +0530

    initial commit

---
+++ init.el
@@ -0,0 +1,67 @@
+;;; init.el --- entry point for Emacs' Preamble configuration.
+;;
+;; Copyright (c) 2013 Nikhil Gupta
+;;
+;; Author: Nikhil Gupta <me@nikhgupta.com>
+;; URL: http://github.com/nikhgupta/preamble
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file simply loads the `emacs.org' file, and runs the code under it.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+
+;;; Requirements:
+
+;; Emacs 24
+
+;;; Code:
+
+(setq preamble-directory (file-name-directory (file-truename load-file-name)))
+(package-initialize)
+
+;; get the org-version but avoid calling org functions to prevent autoloading
+(let ((org-version
+  (with-temp-buffer
+    (require 'find-func)
+    (insert-file-contents (find-library-name "org-version"))
+    (re-search-forward "(org-release \"\\(.*?\\)\")")
+    (match-string 1))))
+
+  ;; update org-mode to v8.0+, if required.
+  (message "Found your org-mode version to be %s." org-version)
+  (if (version< org-version "8.0")
+    (progn
+      (message "Upgrading your org-mode version..")
+      (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+      (package-refresh-contents)
+      (package-install 'org-plus-contrib))
+    (message "Great. We can, now, proceed to literally load our configuration. :)")))
+
+;; load our configuration file
+(add-hook 'after-init-hook
+  (lambda ()
+    (org-babel-load-file
+      (expand-file-name "preamble.org" preamble-directory))))
